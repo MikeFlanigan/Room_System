ce0b61095cd067589ee56c270741aa578f74832a
flanman1866 <flanman1866@gmail.com>
2017-04-25 23:15:20 -0700

    python time keeping with light try one, audio partly added too. Will add a relay to kill speaker buzziness though. Ardu code empty (basically).

---
+++ Room_System_Ardu/Room_System_Ardu.ino
@@ -0,0 +1,20 @@
+#include <FastLED.h>
+
+#define LED_PIN     3
+#define NUM_LEDS    150
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+#define UPDATES_PER_SECOND 100
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Room_System_Rpi.py
@@ -0,0 +1,89 @@
+import datetime
+import timeit
+import subprocess
+import RPi.GPIO as gpio
+
+alarm_time_hour = 5 # 5 am default
+alarm_time_minute = 0
+
+Alarm_Lights = False
+Alarm_Lights_Flash = False
+
+Audio_Enable = True
+Play_Music = False
+Music_Start_OneShot = False
+
+Lights_Pin = 26 
+Flash_Pin = 19
+# _Pin = 13
+gpio.setmode(gpio.BCM) # sets pin reference scheme to gpio instead of pins
+##gpio.setup(MPS_inp, gpio.IN) # N.O.
+##gpio.setup(Start_btn, gpio.IN, gpio.PUD_UP) # N.C. with pull up resistor
+gpio.setup(Lights_Pin, gpio.OUT) # N.O.
+gpio.setup(Flash_Pin, gpio.OUT) # N.O.
+
+while True:
+    Now = datetime.datetime.now()
+    # alarm button
+    # if alarm set button high for
+    alarm_set_time = timeit.default_timer() # begin timer
+    while Alarm_Set_Btn:
+        # enter into alarm set mode
+        if timeit.default_timer()-alarm_set_time > 6000:
+            # enter setting mode
+            # every press moves the alarm time up by 0.25 hours
+            unpressed_time = timeit.default_timer()
+            while timeit.default_timer() - unpressed_time < 20000:
+                if Alarm_Set_Btn:
+                    unpressed_time = timeit.default_timer()
+                    alarm_time_minute += 15
+                    if alarm_time_minute >= 60:
+                        alarm_time_minute = 0
+                        alarm_time_hour += 1
+                    if alarm_time_hour >= 8: # no later than 8 pm
+                        alarm_time_hour = 5
+                        alarm_time_minute = 0
+    # audio button
+    # if audio button latched then enable audio
+    # if audio button unlatched then no audio
+    # if latched and unlatched 4 times in 8 seconds then launch an audio test
+
+
+    # if time t - 5 then set a digital pin high for 5 seconds to tell arduino
+    # to begin lights
+    # if time t = 0 then ''''
+    # if t+ 5 then ''''
+
+    
+    if Now.hour == alarm_time_hour - 1 and Now.minute() >= 55 and alarm_time_minute == 0:
+        Alarm_Lights = True
+    elif Now.hour == alarm_time_hour and Now.minute >= alarm_time_minute-5 and Now.minute<=alarm_time_minute+5:
+        Alarm_Lights = True
+    else: Alarm_Lights = False 
+        
+
+    if Now.hour == alarm_time_hour and Now.minute() >= alarm_time_minute and Now.minute <= alarm_time_minute+5:
+        Alarm_Lights_Flash = True
+        Play_Music = True
+    else:
+        Play_Music = False
+        Alarm_Lights_Flash = False
+
+    if not Alarm_Lights_Flash and not Alarm_Lights and 
+
+
+    # music.kill()
+
+    if Play_Music and not Music_Start_OneShot:
+        Music_Start_OneShot = True
+        music = subprocess.Popen(['mpg123','/home/pi/Desktop/theintro.mp3'])
+    elif not Play_Music and Music_Start_OneShot:
+        music.kill()
+        Music_Start_OneShot = False
+
+    if Alarm_Lights: gpio.output(Lights_Pin, gpio.HIGH)
+    else: gpio.output(Lights_Pin, gpio.LOW)
+    if Alarm_Lights_Flash: gpio.output(Flash_Pin, gpio.HIGH)
+    else: gpio.output(Lights_Pin, gpio.LOW)
+    
+gpio.cleanup()
